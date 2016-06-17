b670f23c0e3e8ad277760a2f8f1384870498ba6a
Rajprasad <rajprasad.kadam@mindtree.com>
2016-06-17 17:57:50 +0530

    Inital Commit for source

---
+++ src/js/actions/Actions.js
@@ -0,0 +1,5 @@
+﻿//actions dispactch to the dispatcher with an action name
+import Dispatcher from "../../dispatcher"
+export function createTodo(text){
+    Dispatcher.dispatch({type:"CREATE_TODO",text})
+}
